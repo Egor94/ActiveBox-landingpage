4835b101615383725ae061a8b0a96834f4b594b4
Egor <egorwolodyko@gmail.com>
2024-05-07 17:48:32 +0200

    create default landing

---
+++ dist/index.09c24910.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(".slider").slick();
+});
+console.log("[jjfdjnd");
+
+//# sourceMappingURL=index.09c24910.js.map
---
+++ dist/index.944d90cd.js
@@ -0,0 +1,2 @@
+console.log("222");
+//# sourceMappingURL=index.944d90cd.js.map
